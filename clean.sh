3decf32233ff1eb626fde2e8d923f2281af65e0a
Lee Turner <lee.turner@crunch.co.uk>
2017-08-23 13:29:08 +0100

    Added file to clean up a vimrc install

---
+++ clean.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+INSTALL_TO=~/dev/tmp
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+clean_vimrc () {
+
+
+    # Remove Symlink ~/.vim and ~/.vimrc
+    echo "Removing your links to tmp vim files......"
+    cd ~
+    rm ./.vimrc
+    rm ./.vim
+
+    echo "Removing your tmp vim files......"
+    rm -rv "$INSTALL_TO/vimrc"
+
+    echo "Your .vim and .vimrc files have been cleaned up."
+}
+
+clean_vimrc
